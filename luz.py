4e0f5543be8113784feeb8f6e942d1fb43f3aa93
luisteixeira13 <lfctdeath@gmail.com>
2023-12-02 20:12:26 -0300

    Modificações no ilumina para receber a requisição

--- luz.py
+++ luz.py
@@ -1,5 +1,7 @@
 import subprocess
-
+import requests
+import time
+API_URL = "http://luisteixeira13.pythonanywhere.com/ilumina/"  # Substitua pelo URL real da sua API
 def set_brightness(brightness):
     # Valores típicos para brightness: 0.1 (mínimo) a 1.0 (máximo)
     brightness = min(1.0, max(0.1, brightness))
@@ -20,5 +22,21 @@ def set_brightness(brightness):
     else:
         print("Não foi possível encontrar o dispositivo de exibição.")
 
-# Ajusta o brilho para 80%
-set_brightness(0.9)
+while True:
+    try:
+        response = requests.get(API_URL)
+        if response.status_code == 200:
+            print(response.json())
+            data = response.json()  # Aqui você acessa os dados JSON
+            presenca = data['mensagem']
+            if presenca == 'Valor de presença: presente':
+                set_brightness(0.9)
+                print("Presente!!!")
+            else:
+                set_brightness(0.2)
+            print("Dados enviados com sucesso para a API!")
+        else:
+            print(f"Falha ao enviar dados para a API. Código de status: {response.status_code}")
+    except Exception as e:
+        print(f"Erro ao enviar dados para a API: {e}")
+    time.sleep(3)
