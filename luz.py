f04a85144b2e6146865fab9e5b644684a40c2dda
luisteixeira13 <lfctdeath@gmail.com>
2023-12-03 21:29:14 -0300

    Testando outro modificador de luz, nova branch

--- luz.py
+++ luz.py
@@ -1,42 +1,42 @@
 import subprocess
 import requests
 import time
-API_URL = "http://luisteixeira13.pythonanywhere.com/ilumina/"  # Substitua pelo URL real da sua API
+
+API_URL = "http://luisteixeira13.pythonanywhere.com/ilumina/"
+
 def set_brightness(brightness):
-    # Valores típicos para brightness: 0.1 (mínimo) a 1.0 (máximo)
-    brightness = min(1.0, max(0.1, brightness))
+    try:
+        brightness = min(100, max(10, int(brightness * 100)))  # Converte para o formato aceito pelo 'light'
 
-    # Obtém a lista de dispositivos de exibição
-    display_list = subprocess.check_output(["xrandr"]).decode("utf-8").splitlines()
+        subprocess.run(["light", "-S", str(brightness)])
 
-    # Procura o nome do dispositivo de exibição (normalmente eDP-1, VGA-1, etc.)
-    display_name = None
-    for line in display_list:
-        if " connected" in line:
-            display_name = line.split()[0]
-            break
+        print(f"Brilho ajustado para {brightness}%")
 
-    if display_name:
-        # Ajusta o brilho usando xrandr
-        subprocess.run(["xrandr", "--output", display_name, "--brightness", str(brightness)])
-    else:
-        print("Não foi possível encontrar o dispositivo de exibição.")
+    except subprocess.CalledProcessError as e:
+        print(f"Falha ao ajustar o brilho: {e}")
+    except Exception as e:
+        print(f"Erro inesperado ao ajustar o brilho: {e}")
 
 while True:
     try:
         response = requests.get(API_URL)
-        if response.status_code == 200:
-            print(response.json())
-            data = response.json()  # Aqui você acessa os dados JSON
-            presenca = data['presença']
-            if presenca == 'presente':
-                set_brightness(0.9)
-                print("Presente!!!")
-            else:
-                set_brightness(0.2)
-            print("Dados enviados com sucesso para a API!")
+        response.raise_for_status()
+
+        data = response.json()
+        presenca = data.get('presença', 'ausente')
+
+        if presenca == 'presente':
+            set_brightness(0.2)
+            print("Presente!!!")
         else:
-            print(f"Falha ao enviar dados para a API. Código de status: {response.status_code}")
+            set_brightness(0.2)
+
+        print("Dados enviados com sucesso para a API!")
+
+    except requests.RequestException as e:
+        print(f"Falha ao enviar dados para a API: {e}")
     except Exception as e:
-        print(f"Erro ao enviar dados para a API: {e}")
+        print(f"Erro inesperado ao enviar dados para a API: {e}")
+
     time.sleep(3)
+
